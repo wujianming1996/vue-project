a83439b5bc2a3d52fc15003ef100cb2ca9412884
wujianming <wujianming1996@gmail.com>
2024-05-02 10:35:21 +0000

    first commit

---
+++ backup/01/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app">
+    <h1>微优易</h1>
+    <h1>微优易</h1>
+    <h1>微优易</h1>
+    <h1>微优易</h1>
+
+</div>
+    
+</template>
+<script lang="ts">
+export default{
+name:'App'
+}
+console.log("vue")
+</script>
+
+<style>
+.app {
+    background-color:bisque;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+
+}
+</style>
---
+++ backup/01/src/main.ts
@@ -0,0 +1,8 @@
+// 引入 createAPP 用于创建应用
+import {createApp} from "vue"
+
+// 引入 APP根组件
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
